bc24f58dd9f19f62631dc274d646e3ad0cb433e9
Robert Frank <trobfrank92@gmail.com>
2017-04-12 15:03:32 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js').then(function(sw) {
-    // registration worked!
-    console.log("Registered Service Worker (sw.js)");
-  }).catch(function() {
-    // registration failed :(
-       console.log("Registration FAILED");
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('Service Worker and Push is supported');
+
+  navigator.serviceWorker.register('sw.js')
+  .then(function(swReg) {
+    console.log('Service Worker is registered', swReg);
+
+    swRegistration = swReg;
+  })
+  .catch(function(error) {
+    console.error('Service Worker Error', error);
   });
+} else {
+  console.warn('Push messaging is not supported');
 }
 
