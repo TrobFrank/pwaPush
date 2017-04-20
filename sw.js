217fe730b87f5735ead07c9b88b4cc8c9b95e67c
Robert Frank <trobfrank92@gmail.com>
2017-04-20 10:11:47 -0400

    added push, added click notification/link

--- sw.js
+++ sw.js
@@ -1,6 +1,22 @@
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
-/*
-self.addEventListener('fetch', function(event) {
-  console.log(event.request);
+  const title = 'Push Lab';
+  const options = {
+    body: 'Yay it works.',
+    icon: 'logo.png',
+    badge: 'logo.png'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
 });
-*/
+
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow('https://www.legendwebworks.com/support/')
+  );
+});
