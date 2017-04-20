4f7dcfb28b9342a0141ed468fa6f5f07a536b928
Robert Frank <trobfrank92@gmail.com>
2017-04-20 12:46:50 -0400

    added abstract

--- sw.js
+++ sw.js
@@ -1,8 +1,20 @@
+// ABSTRACT
+//
+// sw.js will let you display your data to your user.
+// this is where account portal information (ex. user ID, action needed, update made) will be populated. this data may need to be fetched or loaded from json
+// for fetching data properties see: https://www.youtube.com/watch?v=_dXBibRO0SM (~time stamp 30:00)
+//
+// actions are available with custom outcomes. (ex. Yes or No options with specific destinations) 
+//
+// user subscription and authentication takes place in main.js
+// at this point, the service worker is already registered from main.js and waiting for instructions from server to display to user
+//////////////////////////////
+
 self.addEventListener('push', function(event) {
   console.log('[Service Worker] Push Received.');
   console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
-  const title = 'Push Lab';
+  const title = 'Push Lab'; 
   const options = {
     body: 'Yay it works.',
     icon: 'logo.png',
