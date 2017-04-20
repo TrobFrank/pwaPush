ffa71840a7726e6b470d8cb7c2e50ff798649e32
Robert Frank <trobfrank92@gmail.com>
2017-04-20 10:52:44 -0400

    Update desktop-notification.js

--- desktop-notification.js
+++ desktop-notification.js
@@ -1,14 +1,12 @@
 Notification.requestPermission(); //asks users to allow or deny notifications
 
-/*
-var notification = new Notification("Title Value", {
-  body:"This is the body value",
+var notification = new Notification("Desktop Notification", {
+  body:"Only appears on desktop upon visit",
   icon:"logo.png",
-  tag:"type identifier",// use this prop to limit amount of notifications showing (will only show most recent of each tag)
+  tag:"desktop",// use this prop to limit amount of notifications showing (will only show most recent of each tag)
   
 });
 
 notification.onclick = function(){
  location.href = "https://www.legendwebworks.com/support/";
 }
-*/
